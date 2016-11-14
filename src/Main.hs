b1ea3daf9c0b1c63bd886f179835b1f1ca65eb23
Andrey Sverdlichenko <andreys@microsoft.com>
2016-11-14 14:25:02 -0800

    Restartable crawler

--- src/Main.hs
+++ src/Main.hs
@@ -23,7 +23,7 @@ import qualified Data.Set as S
 
 data FetchContext = FetchContext
     { urlQueue :: TVar (Seq String)
-    , activeFetchers :: TVar Int
+    , inProgress :: TVar (S.Set String)
     , visited :: TVar (S.Set String)
     }
 
@@ -36,19 +36,53 @@ main = do
 
 fetch :: String -> FilePath -> IO ()
 fetch urlRoot baseOutputDir = do
+    let contextFile = baseOutputDir </> "#status" 
     manager <- newManager tlsManagerSettings
-    context <- atomically $ do
-                q <- newTVar $ Seq.singleton urlRoot
-                n <- newTVar 0
-                v <- newTVar $ S.singleton urlRoot
-                return FetchContext { urlQueue = q, activeFetchers = n, visited = v }
+    haveSavedContext <- doesFileExist contextFile
+    context <- if haveSavedContext
+                then do
+                    putStrLn "recovering status"
+                    (queue, _ : vis) <- span (/= "") . lines <$> readFile contextFile
+                    atomically $ do
+                        q <- newTVar $ Seq.fromList queue
+                        n <- newTVar $ S.empty
+                        v <- newTVar $ S.fromList vis
+                        return FetchContext { urlQueue = q, inProgress = n, visited = v }
+                else do
+                    putStrLn "no saved status"
+                    atomically $ do
+                        q <- newTVar $ Seq.singleton urlRoot
+                        n <- newTVar $ S.empty
+                        v <- newTVar $ S.singleton urlRoot
+                        return FetchContext { urlQueue = q, inProgress = n, visited = v }
 
     replicateM_ 20 $ forkIO $ worker manager context baseOutputDir
+    writer <- forkIO $
+        let loop = do
+                threadDelay 1000000
+
+                (q, a, v) <- atomically $ do
+                    q <- readTVar (urlQueue context)
+                    a <- readTVar (inProgress context)
+                    v <- readTVar (visited context)
+                    return (q, a, v)
+
+                createDirectoryIfMissing True baseOutputDir
+                withFile (baseOutputDir </> "#new-status") WriteMode $ \h -> do
+                    mapM_ (hPutStrLn h) a
+                    mapM_ (hPutStrLn h) q
+                    hPutStrLn h ""
+                    mapM_ (hPutStrLn h) v
+                renameFile (baseOutputDir </> "#new-status") contextFile
+                loop
+         in loop
 
     atomically $ do
         queueEmpty <- Seq.null <$> readTVar (urlQueue context)
-        nActive <- readTVar $ activeFetchers context
-        unless (queueEmpty && nActive == 0) retry
+        workersInactive <- S.null <$> readTVar (inProgress context)
+        unless (queueEmpty && workersInactive) retry
+
+    killThread writer
 
 worker :: Manager -> FetchContext -> FilePath -> IO ()
 worker manager context baseOutputDir = do
@@ -69,7 +103,7 @@ worker manager context baseOutputDir = do
                         else return vis
                 ) v moreUrls
             writeTVar (visited context) vnext
-            modifyTVar (activeFetchers context) (\n -> n - 1)
+            modifyTVar (inProgress context) (url `S.delete`)
 
         worker manager context baseOutputDir
     where
@@ -78,18 +112,19 @@ worker manager context baseOutputDir = do
         let qview = Seq.viewl q
         case qview of
             EmptyL -> do
-                nActive <- readTVar $ activeFetchers context
-                if nActive /= 0 then retry else return Nothing
+                allInactive <- S.null <$> readTVar (inProgress context)
+                if allInactive then return Nothing else retry
             topUrl :< qnext -> do
                 writeTVar (urlQueue context) qnext
-                modifyTVar (activeFetchers context) (+ 1)
+                modifyTVar (inProgress context) (topUrl `S.insert`)
                 return $ Just topUrl
+
     getAndParse url = do
         request <- parseRequest url
         withResponseHistory request manager $ \histResponse -> do
             let response = hrFinalResponse histResponse
-            let finalUri = getUri $ hrFinalRequest histResponse
-            let baseUri = getUri request
+            let finalUri = fixPort $ getUri $ hrFinalRequest histResponse
+            let baseUri = fixPort $ getUri request
             let status = responseStatus response
             let contentType = responseContentType response
             putStrLn $ show baseUri ++ " -> " ++ show finalUri ++ " " ++ show status ++ " " ++ show contentType
@@ -114,24 +149,42 @@ worker manager context baseOutputDir = do
                         then mapMaybe (fixLink finalUri) . getLinks <$> BL.readFile outputPath
                         else return []
                 else return []
+
     whenJust Nothing _ = return ()
     whenJust (Just a) f = f a
+
     responseContentType = lookup hContentType . responseHeaders
+
     getLinks body =
         let tags = parseTagsOptions parseOptionsFast body
-            anchors = filter (isTagOpenName "a") tags
+            anchors = filter (\t -> isTagOpenName "a" t || isTagOpenName "link" t) tags
          in mapMaybe getHref anchors
+
     getHref (TagOpen "a" attrs) = lookup "href" attrs
-    getHref t = error $ "Internal error: expected TagOpen 'a', got " ++ show t
+    getHref (TagOpen "link" attrs) = lookup "href" attrs
+    getHref t = error $ "Internal error: expected TagOpen 'a' or 'link', got " ++ show t
+
     fixLink base url = do
         uri <- parseURIReference $ BS8.unpack $ BL.toStrict url
         let absoluteUri = if uriIsAbsolute uri then uri else relativeTo uri base
-        if fromSameHost base absoluteUri
-            then return $ normalizePathSegments $ show $ absoluteUri { uriFragment = "" }
+        let fixedScheme = if null (uriScheme absoluteUri)
+                            then absoluteUri { uriScheme = uriScheme base }
+                            else absoluteUri
+        let fixedPortUri = fixPort fixedScheme
+        if fromSameHost base fixedPortUri
+            then return $ normalizePathSegments $ show $ fixedPortUri { uriFragment = "" }
             else Nothing
+
+    fixPort uri = let urischeme = uriScheme uri
+                      uriport = uriPort <$> uriAuthority uri
+                   in if (urischeme == "https:" && uriport == Just ":443") ||
+                            (urischeme == "http:" && uriport == Just ":80")
+                        then uri { uriAuthority = (\a -> a { uriPort = "" }) <$> uriAuthority uri }
+                        else uri
     fromSameHost uri1 uri2 = uriScheme uri1 == uriScheme uri2 && uriAuthority uri1 == uriAuthority uri2
     isText response = (BS8.isPrefixOf "text/" <$> responseContentType response) == Just True
     isHtml response = (BS8.isPrefixOf "text/html" <$> responseContentType response) == Just True
+
     uriToPath uri =
         let base = uriPath uri </> uriQuery uri
             expanded = if last base == '/' then base ++ "index.html" else base
